a4cccd59c58e4b557a83ba1db07e9b335733c31b
Vance Tran <vancetran@users.noreply.github.com>
2021-07-27 20:42:33 -0700

    [main] page component

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,8 @@ function App() {
   return (
     <Page>
       <div className="p-8">
-        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-          Case study
-        </div>
         <p className="block mt-1 text-lg leading-tight font-medium text-black">
           Howdy
-        </p>
-        <p className="mt-2 text-gray-500">
-          Getting a new business off the ground is a lot of hard work. Here are
-          five ideas you can use to find your first customers.
         </p>
       </div>
     </Page>
