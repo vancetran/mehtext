fd84faae5f0035c57416c634e3ddd790ed232bce
Vance Tran <vancetran@users.noreply.github.com>
2021-08-29 13:49:36 -0700

    [main] randomize sarcastic text transform

--- src/utils/textTransform.ts
+++ src/utils/textTransform.ts
@@ -1,13 +1,19 @@
+import { getRandomInt } from "./RNG"
+
 export function capitalize(input: string): string {
   const output = input.toUpperCase()
   return output
 }
 
 // TODO: Could be optimized
-export function sarcastic(input: string): string {
+export function sarcastic(input: string, random: boolean = true): string {
   let output = ''
   const arr = input.split('')
   const arrResult = arr.map((el: string, index: number) => {
+    if(random){
+      debugger
+      return getRandomInt(2) ? el.toUpperCase() : el
+    }
     return index % 2 === 0 ? el.toUpperCase() : el
   })
   output = arrResult.join('')
