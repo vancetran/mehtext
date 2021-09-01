2e7471c025f2843acbe61425686cf24f4236ce1e
Vance Tran <vancetran@users.noreply.github.com>
2021-08-31 18:46:42 -0700

    [main] basic App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test.skip('renders learn react link', () => {
+test('Renders Mehtext title', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const textElement = screen.getByText(/mehtext/i)
+  expect(textElement).toBeInTheDocument()
 })
