4c409ccdb8be8b08ff0ff41a2ec36fc3fddc4091
Vance Tran <vancetran@users.noreply.github.com>
2022-02-23 23:54:47 -0800

    main - adjust default sarcastic to the more common non-random usage

--- src/utils/textTransform.test.ts
+++ src/utils/textTransform.test.ts
@@ -1,27 +1,38 @@
 import { clapback, sarcastic, ssssnake } from './textTransform'
 
-describe('textTransform', ()=>{
-
+describe('textTransform', () => {
   describe('sarcastic', () => {
     test('Each letter is output in predictable alternating case when random is false', () => {
-      expect(sarcastic('word', false)).toBe('WoRd')
+      expect(
+        sarcastic(
+          `I need healthcare because I have cancer and I'm dying`,
+          false
+        )
+      ).toBe(`I nEeD HeAlThCaRe bEcAuSe I hAvE CaNcEr aNd I'm dYiNg`)
     })
   })
 
   describe('clapback', () => {
     test('Each word is separated by a clap emoji', () => {
-      expect(clapback('Stop Emphasizing Your Point by Putting Clap Emojis After Every Word')).toBe('Stop👏Emphasizing👏Your👏Point👏by👏Putting👏Clap👏Emojis👏After👏Every👏Word')
+      expect(
+        clapback(
+          'Stop Emphasizing Your Point by Putting Clap Emojis After Every Word'
+        )
+      ).toBe(
+        'Stop👏Emphasizing👏Your👏Point👏by👏Putting👏Clap👏Emojis👏After👏Every👏Word'
+      )
     })
   })
 
   describe('ssssnake', () => {
     test('Each word beginning with lowercase or uppercase S is prefixed with extra SSS characters', () => {
-      expect(ssssnake(`Tunnel snakes rule, We're the tunnel snakes, That's us, And we rule! Rule!`)).toBe(`Tunnel ssssnakes rule, We're the tunnel ssssnakes, That'ssss us, And we rule! Rule!`)
+      expect(
+        ssssnake(
+          `Tunnel snakes rule, We're the tunnel snakes, That's us, And we rule! Rule!`
+        )
+      ).toBe(
+        `Tunnel ssssnakes rule, We're the tunnel ssssnakes, That'ssss us, And we rule! Rule!`
+      )
     })
   })
-}
-
-
-)
-
-
+})
--- src/utils/textTransform.ts
+++ src/utils/textTransform.ts
@@ -1,20 +1,23 @@
-import { getRandomInt } from "./RNG"
+import { getRandomInt } from './RNG'
 
-export function sarcastic(input: string, random: boolean = true): string {
-  const result = input.split('').map((el: string, index: number): string => {
-    if(random){
-      return getRandomInt(2) ? el.toUpperCase() : el
-    }
-    return index % 2 === 0 ? el.toUpperCase() : el
-  }).join('')
+export function sarcastic(input: string, random: boolean = false): string {
+  const result = input
+    .split('')
+    .map((el: string, index: number): string => {
+      if (random) {
+        return getRandomInt(2) ? el.toUpperCase() : el
+      }
+      return index % 2 ? el.toUpperCase() : el
+    })
+    .join('')
   return result
 }
 
 // https://www.vice.com/en/article/jpyajg/the-clap-and-the-clap-back-how-twitter-erased-black-culture-from-an-emoji
 export function clapback(input: string): string {
   const arr = input.split(' ')
-  const arrResult = arr.map( (el: string, index: number): string => {
-    if(index !== 0){
+  const arrResult = arr.map((el: string, index: number): string => {
+    if (index !== 0) {
       return `👏${el}`
     } else {
       return el
@@ -25,28 +28,25 @@ export function clapback(input: string): string {
 
 export function ssssnake(input: string): string {
   const arr = input.split(' ')
-  const arrResult = arr.map( (el: string): string => {
+  const arrResult = arr.map((el: string): string => {
     const lastIndex = el.length - 1
-    if(el.charAt(0) === 's' || el.charAt(0) === 'S' ){
-      if(el.charAt(0)==='s')
-        return `sss${el}`
+    if (el.charAt(0) === 's' || el.charAt(0) === 'S') {
+      if (el.charAt(0) === 's') return `sss${el}`
       return `SSS${el}`
     }
-    if(el.charAt(lastIndex) === 's' || el.charAt(lastIndex) === 'S') {
-      if(el.charAt(lastIndex)==='s')
-        return `${el}sss`
+    if (el.charAt(lastIndex) === 's' || el.charAt(lastIndex) === 'S') {
+      if (el.charAt(lastIndex) === 's') return `${el}sss`
       return `${el}SSS`
     }
-      return el
+    return el
   })
   return arrResult.join(' ')
 }
 
 export function airquotes(input: string): string {
-  if(input === '')
-    return ''
+  if (input === '') return ''
   const arr = input.split(' ')
-  const arrResult = arr.map( (el: string, index: number): string => {
+  const arrResult = arr.map((el: string, index: number): string => {
     return `✌${el}✌ `
   })
   return arrResult.join('')
